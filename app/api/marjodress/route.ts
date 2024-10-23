c3c1907b583fcc961c94e940faeb6e84eda5fda9
yovanamg <yovanamata94@gmail.com>
2024-10-23 02:19:50 -0700

    Home listo3

--- app/api/marjodress/route.ts
+++ app/api/marjodress/route.ts
@@ -1,15 +1,15 @@
-import { error } from "console";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-type ResponseData = {
-    message: string;
-};
 
-export async function GET(req:Request) {
-    try {
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { NextResponse } from "next/server";
+// type ResponseData = {
+//     message: string;
+// };
+
+// export async function GET(req:Request) {
+//     try {
         
-        return NextResponse.json({ message: "Exit" }, { status: 200 });
-    } catch(error) {
-        return NextResponse.json({ error: error }, { status: 500 });
-    }
-}
+//         return NextResponse.json({ message: "Exit" }, { status: 200 });
+//     } catch(error) {
+//         return NextResponse.json({ error: error }, { status: 500 });
+//     }
+// }
